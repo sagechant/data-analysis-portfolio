4368eaaa64714e98e474e7b32ee62c6c83c00a49
Arun <arunpvenu910@gmail.com>
2025-11-23 21:07:56 +0200

    Delete myfirstscript.py

--- myfirstscript.py
+++
@@ -1,8 +0,0 @@
-# This is a comment in Python
-print("Hello, this is my first Python script!")
-
-# A basic calculation
-a = 5
-b = 10
-sum_result = a + b
-print(f"The sum of {a} and {b} is {sum_result}")
